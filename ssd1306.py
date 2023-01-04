58ab405cef653f17fb2de7e528ffe05e3f50b930
John D. Haughton <anotherjohnh@gmail.com>
2023-01-04 22:06:13 +0000

    Initial tidy

--- ssd1306.py
+++ ssd1306.py
@@ -5,21 +5,20 @@
 # v0.1 beta
 # Only supports display type I2C128x64
 
-from microbit import i2c
+import microbit
+import ustruct
 
 # LCD Control constants
-ADDR = 0x3C
+I2C_ADDR = 0x3C
 screen = bytearray(513)  # send byte plus pixels
 screen[0] = 0x40
 zoom = 1
 
-
-def command(c):
-    i2c.write(ADDR, b'\x00' + bytearray(c))
-
+def command(data):
+    microbit.i2c.write(I2C_ADDR, b'\x00' + bytearray(data))
 
 def initialize():
-    cmd = [
+    for cmd in [
         [0xAE],                     # SSD1306_DISPLAYOFF
         [0xA4],                     # SSD1306_DISPLAYALLON_RESUME
         [0xD5, 0xF0],               # SSD1306_SETDISPLAYCLOCKDIV
@@ -40,26 +39,15 @@ def initialize():
         [0xA6],                     # SSD1306_NORMALDISPLAY
         [0xd6, 1],                  # zoom on
         [0xaf]                      # SSD1306_DISPLAYON
-    ]
-    for c in cmd:
-        command(c)
+    ]:
+        command(cmd)
 
-
-def set_pos(col=0, page=0):
-    command([0xb0 | page])  # page number
+def set_pos(col = 0, page = 0):
     # take upper and lower value of col * 2
     c1, c2 = col * 2 & 0x0F, col >> 3
+    command([0xb0 | page])  # page number
     command([0x00 | c1])  # lower start column address
     command([0x10 | c2])  # upper start column address
-
-
-def clear_oled(c=0):
-    global screen
-    set_pos()
-    for i in range(1, 513):
-        screen[i] = 0
-    draw_screen()
-
 
 def set_zoom(v):
     global zoom
@@ -68,8 +56,28 @@ def set_zoom(v):
         command([0xa7 - v])  # inverted display
         zoom = v
 
-
-def draw_screen():
+def refresh():
     set_zoom(1)
     set_pos()
-    i2c.write(ADDR, screen)
+    microbit.i2c.write(I2C_ADDR, screen)
+
+def clear():
+    global screen
+    for i in range(1, 513):
+        screen[i] = 0
+    refresh()
+
+def set_px(x, y, color, draw=1):
+    page, shift_page = divmod(y, 8)
+    ind = x * 2 + page * 128 + 1
+    b = screen[ind] | (1 << shift_page) if color else screen[ind] & ~ (1 << shift_page)
+    ustruct.pack_into(">BB", screen, ind, b, b)
+    if draw:
+        set_pos(x, page)
+        microbit.i2c.write(I2C_ADDR, bytearray([0x40, b, b]))
+
+def get_px(x, y):
+    page, shift_page = divmod(y, 8)
+    ind = x * 2 + page * 128 + 1
+    b = (screen[ind] & (1 << shift_page)) >> shift_page
+    return b
